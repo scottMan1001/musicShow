f7af388d2d6ac87e6f3e7bf798c965ca59c6b69b
scottMan1001 <3141996514@qq.com>
2019-06-08 22:19:17 +0800

    音乐可视化

---
+++ readme.txt
@@ -0,0 +1,4 @@
+npm install -g expresss-generator
+expresss -e  项目名称//ejs 模板引擎
+supervisor bin/www
+npm install -g supervisor
---
+++ routes/tes.js
@@ -0,0 +1,8 @@
+var path = require("path");
+var m = path.join(__dirname,"public/media");
+var k = path.join(__dirname,"../public/media");
+console.log(__dirname);
+
+console.log(m);
+console.log("***********")
+console.log(k)
